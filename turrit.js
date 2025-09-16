5a2648e459b7b18da538e8288b4c6a22d20a60ff
zorro-cell <zrrsntg@gmail.com>
2025-09-16 13:44:18 +0800

    Update turrit.js

--- turrit.js
+++ turrit.js
@@ -1,17 +1,5 @@
-#!name = Turrit
-#!desc = Telegram外部链接跳转至Turrit
-#!openUrl = https://apps.apple.com/app/id6471781238
-#!author = sooyaaabo[https://github.com/sooyaaabo]
-#!tag = 功能增强
-#!system = 
-#!system_version = 
-#!loon_version = 3.3.3(894)
-#!homepage = https://github.com/sooyaaabo/Loon/blob/main/README.md
-#!icon = https://raw.githubusercontent.com/sooyaaabo/Loon/main/Icon/App-Icon/Turrit.png
-#!date = 2025-09-09 22:00
+[rewrite_local]
+^https?:\/\/t\.me\/(.+) url 302 turrit://resolve?domain=$1
 
-[Rewrite]
-(https:\/\/)?t\.me\/(.+) 302 turrit://resolve?domain=$2
-
-[MitM]
-hostname = t.me
+[mitm]
+hostname = %APPEND% t.me
